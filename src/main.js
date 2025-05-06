20f082938fb9c2ab70e9e32784cd305f6b0f7ca1
samplecode17 <ibrahim_mou17@hotmail.com>
2025-05-06 23:15:23 +0200

    feat: connect with backend

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import axios from 'axios';
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router';
+import store from './store';
+
+
+const app = createApp(App);
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:8000/';  // the FastAPI backend
+
+axios.interceptors.response.use(undefined, function (error) {
+  if (error) {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      store.dispatch('logOut');
+      return router.push('/login')
+    }
+  }
+});
+
+app.use(router);
+app.use(store);
+app.mount("#app");
