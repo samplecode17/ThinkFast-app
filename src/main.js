bc84b96954e739e312c8015eb964d4bf20bafd60
samplecode17 <ibrahim_mou17@hotmail.com>
2025-05-17 20:08:33 +0200

    feat: add home page and nav-bar component

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 import App from './App.vue'
 import router from './router';
-import store from './store';
+
 
 
 const app = createApp(App);
@@ -13,17 +13,17 @@ const app = createApp(App);
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://localhost:8000/';  // the FastAPI backend
 
-axios.interceptors.response.use(undefined, function (error) {
-  if (error) {
-    const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      store.dispatch('logOut');
-      return router.push('/login')
-    }
-  }
-});
+// axios.interceptors.response.use(undefined, function (error) {
+//   if (error) {
+//     const originalRequest = error.config;
+//     if (error.response.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
+//       store.dispatch('logOut');
+//       return router.push('/login')
+//     }
+//   }
+// });
 
 app.use(router);
-app.use(store);
+// app.use(store);
 app.mount("#app");
