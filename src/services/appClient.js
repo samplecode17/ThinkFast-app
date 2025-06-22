6dd7309c300f490bbf1d09dd007421eceb3ed137
samplecode17 <ibrahim_mou17@hotmail.com>
2025-06-22 20:32:52 +0200

    fix: solve cors bearer not viewed

--- src/services/appClient.js
+++ src/services/appClient.js
@@ -1,12 +1,22 @@
 import axios from "axios";
 
-const backendAPI = import.meta.env.VITE_BACKEND_CONNECTION
+const backendAPI = import.meta.env.VITE_BACKEND_CONNECTION;
 
 const apiClient = axios.create({
-    baseURL: backendAPI,
-    withCredentials: true,
-  });
+  baseURL: backendAPI,
+  withCredentials: true,
+});
 
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  } else {
+    delete config.headers.Authorization;
+  }
+
+  return config;
+});
 
 export default apiClient;
